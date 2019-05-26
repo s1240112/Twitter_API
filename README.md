74a24a317ac5438d572e1778c7de825ab4c9b718
s1240112 <s1240112@u-aizu.ac.jp>
2019-05-27 03:02:59 +0900

    Initialize

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Twitter-APIの取得方法については、documentフォルダーの中に資�
 ## Reference
 1. [IntelliJ IDEAでNode.jsプラグインをインストールする話](https://www.1ft-seabass.jp/memo/2013/11/16/intellij-idea-node-js-plugin-install/)
 2. [Node.js入門 - とほほのWWW入門](http://www.tohoho-web.com/ex/nodejs.html)
+3. [Node.jsでTwitter APIを使ってツイートを取得(APIの初期設定からやってみた)](http://shomi3023.com/2018/01/21/twitter-api-get-tweet/)
+4. [Twitter REST APIの使い方 (Twitter APIの一覧及びパラメーターの種類が載っています)](https://syncer.jp/Web/API/Twitter/REST_API/)
+5. [Twitter 開発者 ドキュメント日本語訳 (Twitter-API一覧が載っています)](http://westplain.sakuraweb.com/translate/twitter/Documentation/REST-APIs/Public-API/GET-statuses-mentions_timeline.cgi)
---
+++ src/twitter/get_tweet.js
@@ -0,0 +1,36 @@
+/*
+    ターミナル（windowsの場合はコマンドプロンプト）上で、
+    screen_nameパラメータや user_idパラメータで指定した
+    ユーザーが投稿した最新のツイート集を取得。
+
+    customer_key, customer_secret, access_token_key, access_token_secret は
+    ご自身のトークンキーを入れてください。
+ */
+
+var twitter = require('twitter');
+var client = new twitter({
+    consumer_key:        'Input Your Customer Key',
+    consumer_secret:     'Input Your Customer Secret Key',
+    access_token_key:    'Input Your Access Token Key',
+    access_token_secret: 'Input Your Access Token Secret Key',
+});
+
+getTweet();
+
+function getTweet() {
+    var params = {
+        screen_name: 'Twitter',
+        count: 10,
+        include_rts: false,
+        exclude_replies: true
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
